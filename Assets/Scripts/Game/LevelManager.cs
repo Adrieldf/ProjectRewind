7515891e99da0944f599256ebc0f4294c92b0ba1
TiagoCastilhos <tiagocastilhos66@gmail.com>
2020-08-03 01:05:22 -0300

    Created endphase

---
+++ Assets/Scripts/Game/LevelManager.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    public void LevelCompleted()
+    {
+        Debug.Log("Parabains");
+        //Carregar o level
+    }
+}
