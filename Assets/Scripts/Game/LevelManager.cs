e4baccb7607a2fdbfaf24f966041417ead0f8487
Adrieldf <adriel.idf@gmail.com>
2020-08-03 23:33:20 -0300

    level manager

--- Assets/Scripts/Game/LevelManager.cs
+++ Assets/Scripts/Game/LevelManager.cs
@@ -4,9 +4,36 @@ using UnityEngine;
 
 public class LevelManager : MonoBehaviour
 {
+    [SerializeField]
+    private List<GameObject> Levels = new List<GameObject>();
+    private int _currLevelIndex = 0;
+
+
+
+    public void SpawnLevel()
+    {
+        Instantiate(Levels[_currLevelIndex], Vector3.zero, Quaternion.identity);
+        _currLevelIndex++;
+
+        
+    }
+    public void ResetLevel()
+    {
+        DeleteCurrentLevel();
+        SpawnLevel();
+
+    }
+    public void DeleteCurrentLevel()
+    {
+        Destroy(GameObject.FindGameObjectWithTag("Level"));
+        _currLevelIndex--;
+    }
+
     public void LevelCompleted()
     {
         Debug.Log("Parabains");
         //Carregar o level
     }
+
+
 }
