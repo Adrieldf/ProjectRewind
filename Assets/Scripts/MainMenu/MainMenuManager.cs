54e1137f25da3b9ed65e2064caacd33fc61ace80
TiagoCastilhos <tiagocastilhos66@gmail.com>
2020-08-06 19:31:19 -0300

    Created main menu Created lamp sprite

---
+++ Assets/Scripts/MainMenu/MainMenuManager.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour
+{
+    public void Play()
+        => SceneManager.LoadScene("Game");
+
+    public void Quit()
+        => Application.Quit();
+}
